776d6acd5411de13d06506bdf1ca1be42857ec35
Alessandro Traspadini <traspadini@dei.unipd.it>
2023-06-06 13:23:40 +0200

    Add command-line parsing parameters

--- pyshark/pyCapture.py
+++ pyshark/pyCapture.py
@@ -1,23 +1,38 @@
 # PyShark documentation: http://kiminewt.github.io/pyshark/
+# run example: python pyCapture.py test 2.8.2 684336
+
 
 import pyshark
 import csv
 import datetime
 from progress.bar import IncrementalBar
+import argparse
+
+
+# Construct the argument parser
+ap = argparse.ArgumentParser()
+
+# Add the arguments to the parser
+ap.add_argument("fileName", help="pcap file name")
+ap.add_argument("vrAddress", help="address of the VR from the pcap")
+ap.add_argument("totPackets", help="Total number of packets on the pcap file", type=int)
+
+args = ap.parse_args()
 
 # update accordingly
-capture = pyshark.FileCapture('test.pcap') #Input .pcap file
-csvFile = "trace.csv" # Output file
+inputTrace = args.fileName + ".pcapng"
+outputFile = args.fileName + "_filtered.csv"
+capture = pyshark.FileCapture(inputTrace) #Input .pcap file
 srcAddress = "host" # Computer Address
-dstAddress = "2.8.2" # VR Address
-totPackets = 684336 # Total number of packets on the .pcap file
+dstAddress = args.vrAddress # VR Address
+totPackets = args.totPackets # Total number of packets on the .pcap file
 
 def timestamp(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     return (dt - epoch).total_seconds() * 1000.0
 
 with IncrementalBar('Processing...', max = totPackets, suffix='%(percent).1f%% - %(elapsed)ds') as bar:
-    with open(csvFile, "w") as output:
+    with open(outputFile, "w") as output:
         header = "time,size,direction \n"
         output.write(header)
         for packet in capture:
